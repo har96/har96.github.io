4a5b22c8c9ee3531bdc29dd7b1ec3cef2cab8b22
Harrison Hunter <harrisonhunter27@gmail.com>
2022-10-26 14:35:43 -0500

    Add Projects section along with projects component

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Project, PROJECTS } from "./project";
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss']
+})
+export class ProjectsComponent implements OnInit {
+
+  projects: Project[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+      this.projects = PROJECTS;
+      this.projects = this.projects.sort((a: Project, b: Project) => b.magnitude - a.magnitude);
+  }
+
+  getCategoryClass(category: string): string {
+      switch (category) {
+          case "Work":
+              return "btn-success";
+          case "Personal":
+              return "btn-primary";
+          case "Academic":
+              return "btn-dark";
+          default:
+              return "btn-light";
+      }
+  }
+
+}
