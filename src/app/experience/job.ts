d161525ccbe3121b9fdaecbd048cc499843a7d9d
Harrison Hunter <robert.hunter@3ds.com>
2023-04-03 14:19:41 -0500

    Add DS job experience

--- src/app/experience/job.ts
+++ src/app/experience/job.ts
@@ -22,10 +22,19 @@ Programmed for research teams full-time every summer and part-time for one year 
           employer: "US Army Corp of Engineers",
           skills: ["Python", "C", "C++", "Bash", "Unix/Linux", "HTML/Javascript", "Machine Learning", "Image Processing", "Graphics Card Programming", "SQL", "Excel"],
           start_date: new Date("2019-01-14"),
-          end_date: new Date(0),
+          end_date: new Date("2022-12-02"),
           description: `
 Specialized in parallelizing C and C++ simulation software for High Performance Computing, developing data processing pipelines and visualizations with Python, automating shell workflows in Linux/Unix, and writing technical publications. Published seven peer-reviewed papers, four as first author. Presented my research at two conferences as well as numerous internal briefings and project showcases.
 
 Assisted in budget management of project funds.`
+      },
+      {
+          title: "Software Engineer",
+          employer: "Dassault Systemes",
+          skills: ["C++", "Unix/Linux", "Google Test"],
+          start_date: new Date("2022-12-05"),
+          end_date: new Date(0),
+          description: `
+Developed commercial C++ software for the DS ENOVIA brand.`
       }
 ];
